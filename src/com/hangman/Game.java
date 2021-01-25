6cc01ee8e00938a2d9c2f1fe925e4df27f63e822
KWA <slimak-75@wp.pl>
2021-01-25 15:22:46 +0100

    Initial commit

---
+++ src/com/hangman/Game.java
@@ -0,0 +1,47 @@
+package com.hangman;
+
+import java.util.Scanner;
+
+public class Game {
+    private String[] words = {"terminator", "banana", "computer", "cow", "rain", "water" };
+    private String word = words[(int) (Math.random() * words.length)];
+    StringBuilder sb = new StringBuilder(new String(new char[word.length()]).replace("\0", "_"));
+    private  int count = 0;
+
+    public void startGame() {
+        Scanner sc = new Scanner(System.in);
+        boolean answer = false;
+        String guess;
+        System.out.println("Guess word");
+        System.out.println(sb.toString() + " (" + word.length() + ")");
+        while (count < 3) {
+            guess = sc.next();
+            answer = checkWord(guess);
+            if (answer)
+                break;
+        }
+        if (answer == false)
+            System.out.println("Too many wrong answers, The word was: " + word);
+    }
+    private boolean checkWord(String guess) {
+        if (guess.equals(word)) {
+            System.out.println("Correct! You win! The word was: " + word);
+            return true;
+        }
+        System.out.println("Wrong answer");
+        count++;
+        printOneLetter();
+        return false;
+    }
+    private void printOneLetter() {
+        int letter = 0;
+        boolean cantPrint = true;
+        while (cantPrint) {
+            letter = (int) (Math.random() * word.length());
+            if (sb.charAt(letter) == '_')
+                cantPrint = false;
+        }
+        sb.setCharAt(letter, word.charAt(letter));
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/com/hangman/Main.java
@@ -0,0 +1,13 @@
+package com.hangman;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        while(true) {
+            Game game = new Game();
+            game.startGame();
+        }
+    }
+}
